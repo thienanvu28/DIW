db7fddc0aaa981235bdd43b1ebc3f5d68c766cd3
thienanvu28 <95689366+thienanvu28@users.noreply.github.com>
2022-01-18 18:06:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+// typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Pratice, Create, Assign, Publish"],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+    });
